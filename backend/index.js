7bcb1497f45ecdfd985999d486c3da10dead0e45
Sigeth <52868615+Sigeth@users.noreply.github.com>
2023-01-19 02:33:41 +0100

    feat: added lists in database

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,13 @@ const app = express();
 const session = require("express-session");
 const port = 3000;
 const { getTasks, deleteTaskByID, insertNewTask, updateTaskByID } = require("./taskController.js");
+const { getLists, deleteListByID, addList, updateListByID } = require("./listController");
 const { registerUser, findUser } = require("./userController.js");
 const cors = require("cors");
+const { ObjectId } = require("mongodb");
 
 app.use(cors({credentials: true, origin: "http://localhost:4200"}))
 app.use(express.json());
-app.use(express.static(__dirname + '/website'));
 app.use(session({
     secret: "secretStringIGuess",
     name: "cookieTaskApp"
@@ -26,9 +27,9 @@ function checkSignedIn(req, res, next) {
 ##########################################
 */
 
-app.get("/api/tasks", checkSignedIn, async (req, res, next) => {
+app.get("/api/tasks", async (req, res, next) => {
     try {
-        const tasks = await getTasks();
+        const tasks = await getTasks(req.query.listId);
         res.status(200).json(tasks);
     } catch (e) {
         console.error(e);
@@ -51,7 +52,8 @@ app.post("/api/task", async (req, res, next) => {
         const task = {
             title: req.body.title,
             finished: req.body.finished,
-            status: req.body.status
+            status: req.body.status,
+            listId: new ObjectId(req.body.listId)
         };
         await insertNewTask(task);
 
@@ -70,6 +72,60 @@ app.put("/api/task/:id", async (req, res, next) => {
             statusMsgs.push(await updateTaskByID(req.params.id, key, req.body[key]));
         }
         res.status(200).json({message: "Task updated", status: statusMsgs});
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({code: 500, error: e.toString()});
+    }
+});
+
+/*
+##########################################
+############ LIST ENDPOINTS ##############
+##########################################
+*/
+
+app.get("/api/lists", async (req, res, next) => {
+    try {
+        const lists = await getLists();
+        res.status(200).json(lists);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({code: 500, error: e.toString()});
+    }
+});
+
+app.delete("/api/list/:id", async (req, res, next) => {
+    try {
+        const statusMsg = await deleteListByID(req.params.id);
+        res.status(200).json({message: "List deleted", status: statusMsg});
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({code: 500, error: e.toString()});
+    }
+});
+
+app.post("/api/list", async (req, res, next) => {
+    try {
+        const list = {
+            title: req.body.title
+        };
+        await addList(list);
+
+        res.status(200).json(list);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({code: 500, error: e.toString()});
+    }
+});
+
+app.put("/api/list/:id", async (req, res, next) => {
+    try {
+        let statusMsgs = []
+        for (const key of Object.keys(req.body)) {
+            if (key === "_id") continue;
+            statusMsgs.push(await updateListByID(req.params.id, key, req.body[key]));
+        }
+        res.status(200).json({message: "List updated", status: statusMsgs});
     } catch (e) {
         console.error(e);
         res.status(500).json({code: 500, error: e.toString()});
--- backend/listController.js
+++ backend/listController.js
@@ -2,27 +2,27 @@ const { MongoClient, ObjectId } = require("mongodb");
 const dbUrl = "mongodb://127.0.0.1:27017";
 
 module.exports = {
-    async getTasks(listId) {
+    async getLists() {
         const db = await MongoClient.connect(dbUrl)
         const dbo = db.db("tasks");
 
-        return await dbo.collection("tasks").find({listId: new ObjectId(listId)}).toArray();
+        return await dbo.collection("lists").find().toArray();
     },
-    async insertNewTask(task) {
+    async addList(list) {
         const db = await MongoClient.connect(dbUrl);
         const dbo = db.db("tasks");
-        return await dbo.collection("tasks").insertOne(task);
+        return await dbo.collection("lists").insertOne(list);
     },
-    async deleteTaskByID(id) {
+    async deleteListByID(id) {
         const db = await MongoClient.connect(dbUrl)
         const dbo = db.db("tasks");
-        return await dbo.collection("tasks").deleteOne({_id: new ObjectId(id)});
+        return await dbo.collection("lists").deleteOne({_id: new ObjectId(id)});
     },
-    async updateTaskByID(id, key, newValue) {
+    async updateListByID(id, key, newValue) {
         const db = await MongoClient.connect(dbUrl);
         const dbo = db.db("tasks");
         let toChange = {};
         toChange[key] = newValue;
-        return await dbo.collection("tasks").updateOne({_id: new ObjectId(id)}, {$set: toChange});
+        return await dbo.collection("lists").updateOne({_id: new ObjectId(id)}, {$set: toChange});
     }
 }
