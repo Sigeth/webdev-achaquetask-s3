203214fdec27d08ee3ef74fe7d73f7f229f303e7
Sigeth <52868615+Sigeth@users.noreply.github.com>
2023-01-20 00:21:05 +0100

    fix: deleting list now deletes all tasks linked to it

--- backend/listController.js
+++ backend/listController.js
@@ -16,6 +16,7 @@ module.exports = {
     async deleteListByID(id) {
         const db = await MongoClient.connect(dbUrl)
         const dbo = db.db("tasks");
+        await dbo.collection("tasks").deleteMany({listId: new ObjectId(id)});
         return await dbo.collection("lists").deleteOne({_id: new ObjectId(id)});
     },
     async updateListByID(id, key, newValue) {
